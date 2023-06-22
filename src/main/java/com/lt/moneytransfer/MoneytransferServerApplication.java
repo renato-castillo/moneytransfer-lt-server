5db773b8d14cdf0d6bcfe24e4fbb61dca96db135
rcastillo <renatocastillopingo@gmail.com>
2023-06-22 02:09:16 -0500

    First commit

---
+++ src/main/java/com/lt/moneytransfer/MoneytransferServerApplication.java
@@ -0,0 +1,13 @@
+package com.lt.moneytransfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneytransferServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneytransferServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lt/moneytransfer/MoneytransferServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lt.moneytransfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneytransferServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
